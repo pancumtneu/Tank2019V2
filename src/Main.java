9e52fa5f476e723d76dd6a8c3a3d54914d0e95a0
pancumtneu <carvip725@163.com>
2020-07-12 18:33:32 +0800

    绘制移动的坦克

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         TankFrame tf=new TankFrame();
         tf.setVisible(true);
-    }
+
+
+
+            for(;;) {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                tf.repaint();  //隔一段时间重绘一次TankFrame  这样能够重新
+            }
+
+
+        }
 }
--- src/TankFrame.java
+++ src/TankFrame.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 
 public class TankFrame extends Frame {
-    private int x=100;
+    private int x=100,y=100;
 
     public TankFrame()  {
         this.setTitle("Tank war!");
@@ -9,8 +9,32 @@ public class TankFrame extends Frame {
         this.setSize(800,600);
 
     }
+
     @Override
-    public void paint(Graphics g){
-        g.fillRect(10,20,30,40);
+   public void paint(Graphics g) {
+        //g.fillRect(x,y,30+x,40);贪吃蛇这么移动
+        //g.fillRect(10,y,30+x,40);进度条这么移动
+        g.fillRect(x,y,30+x,40);
+        x++;
     }
+
+
+
+
+//    @Override
+//    public void paint(Graphics g){
+//
+//        for(;;){
+//            try {
+//                Thread.sleep(200);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            g.fillRect(x,y,30,40);
+//            x++;
+//            repaint();
+//        }
+
+
+
 }
